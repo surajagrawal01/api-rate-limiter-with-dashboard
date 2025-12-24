119f1666c7ba36157c76d5b1cdeacb83e1a70df2
surajagrawal01 <surajagrawal70500@gmail.com>
2025-12-24 22:51:25 +0530

    feat(middleware): add rate limiter with usage and plan checks
    - validate API key and user
    - enforce plan limits and time window
    - handle window expiry and usage increment
    - log allowed and blocked requests

---
+++ backend/src/middleware/rateLimiter.ts
@@ -0,0 +1,63 @@
+import { insertLog } from "../services/logService";
+import { checkUserAllowedBasedOnCount, checkWindowExpiredOrNot, createUsageForUser, getUsageForUser, increaseUsageForUser, resetUsageForUser } from "../services/usageService";
+import { findUserByAPIKey } from "../services/userService"
+import { Request, Response, NextFunction } from "express";
+
+export const middleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const apiKey = req.headers["x-api-key"] as string;
+        if (!apiKey) {
+            return res.status(401).json({ status: "BLOCKED", error: "API key missing" });
+        }
+        const endPoint = req.originalUrl;
+        const method = req.method;
+
+        const user = await findUserByAPIKey(apiKey)
+        console.log("🚀 ~ middleware ~ user:", user)
+        let userAllowed = false
+        if (!user) throw new Error("User not found")
+        const plan = user.plan;
+        if (!plan) throw new Error("no plan associated")
+        const usage = await getUsageForUser(user?.id)
+        console.log("🚀 ~ middleware ~ usage:", usage)
+        if (!usage) {
+            const newUsage = await createUsageForUser({
+                userId: user?.id,
+                used: 1,
+                window_seconds: plan?.window_seconds
+            })
+        }
+        const windowExpired = await checkWindowExpiredOrNot(user?.id)
+        if (windowExpired) {
+            await resetUsageForUser(user?.id)
+            userAllowed = true
+        } else {
+            userAllowed = await checkUserAllowedBasedOnCount(user?.id, plan?.limit)
+        }
+
+        if (userAllowed) {
+            await increaseUsageForUser(user?.id)
+            await insertLog({
+                userId: user?.id,
+                endPoint,
+                status: "SUCCESS",
+                method,
+                description: "allowed"
+            })
+            next()
+        } else {
+            await insertLog({
+                userId: user?.id,
+                endPoint,
+                status: "BLOCKED",
+                method,
+                description: "userNotAllowed"
+            })
+            res.json({ error: 401, status: "BLOCKED" })
+        }
+    } catch (err) {
+        console.log(err)
+        return null
+    }
+}
