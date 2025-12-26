17abab8d0f71676f36f4a6752691c7a0dc11a903
surajagrawal01 <surajagrawal70500@gmail.com>
2025-12-26 07:24:57 +0530

    update(index):update to integrate graphql-yoga listener and the router with express setup for listening request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import express from "express"
+import { yoga } from './graphql/yoga';
+import { routerApi } from './router/router';
+const PORT = process.env.PORT || 8002
+const app = express()
 
+//For GraphQL
+app.use('/graphql', yoga)
 
+//For rest apis
+app.use('/api', routerApi)
 
-console.log("Backend running...");
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
